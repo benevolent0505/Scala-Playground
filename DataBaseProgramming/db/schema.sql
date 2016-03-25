3c6458f82e0d2e4337a8e78e263ffa815c651fc7
benevolent0505 <mikio.fjt@gmail.com>
2016-03-25 14:54:01 +0900

    SQL分を記述したファイルを追加

---
+++ DataBaseProgramming/db/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE artist(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT,birthday TEXT);
+
+INSERT INTO artist (id, name, birthday) VALUES (1, '初音ミク', '2007-08-31');
+
+INSERT INTO artist (id, name, birthday) VALUES (2, '鏡音リン', '2007-12-27');
+
+INSERT INTO artist (id, name, birthday) VALUES (3, '鏡音レン', '2007-12-27');
+
+INSERT INTO artist (id, name, birthday) VALUES (4, '巡音ルカ', '2009-01-30');
+
+INSERT INTO artist (id, name, birthday) VALUES (5, '重音テト', '2008-04-01');
